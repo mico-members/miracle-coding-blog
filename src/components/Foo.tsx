a84f2774c0d3101ff68fafdbdaf96b2e44864e90
Dongmin Ahn <68339352+eamon3481@users.noreply.github.com>
2021-08-02 17:39:51 +0900

    [#7] Feat: filter 에 필요한 atom 생성과 seletor를 이용한 비동기 요청 (#8)

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -1,37 +1,27 @@
 import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import { articleListAtom } from '../config/atoms';
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { articleListAtom, currentPage, fetchData, filterIndexAtom } from '../config/store/store';
 import { IPR } from '../config/types/dataTypes';
 
 const Foo = () => {
-  const [articleList, setArticleList] = useRecoilState(articleListAtom);
-  useEffect(() => {
-    const bodyParser = ({ body, base, user, number }: IPR) => {
-      const info = body.split('|');
-      if (info.length !== 16) return { id: -1 };
-      const link = info[14].match(/\([^)]+\)/)?.[0].replace(/[\(\)]/g, '');
-      return {
-        id: number,
-        link: link || '잘못된 링크입니다.',
-        condition: parseInt(info[13].trim()),
-        userImgUrl: user.avatar_url,
-        userName: base.ref,
-        date: info[11],
-      };
-    };
+  const [page, setPage] = useRecoilState(currentPage)
+  const [filterIndex, setFilterIndex] = useRecoilState(filterIndexAtom)
+  const seletor = useRecoilValueLoadable(fetchData)
+  
+  const click = (num:number) => {
+    setFilterIndex(arr=>[...arr, num])
+    console.log(seletor.contents)
+    
+  }
+  return <div>
+    <div id="3" onClick={()=>click(3)}>plus adela3</div>
+    <div id="1" onClick={()=>click(1)}>plus daisy1</div>
+    <div id="0" onClick={()=>click(0)}>plus Q0</div>
+    <div id="reset" onClick={()=>setFilterIndex([])}>reset</div>
+    {seletor.state === "hasValue" && seletor.contents.map(({id}:{id:number})=><div>{id}</div>)}
 
-    fetch(
-      'https://api.github.com/repos/GleamingStar/miracle-coding/pulls?state=closed',
-    )
-      .then((res) => res.json())
-      .then((json) =>
-        setArticleList(
-          json.map(bodyParser).filter(({ id }: { id: number }) => id !== -1),
-        ),
-      );
-  }, []);
 
-  return <div onClick={() => console.log(articleList)}>123</div>;
+  </div>;
 };
 
 export default Foo;
---
+++ src/config/store/store.tsx
@@ -0,0 +1,83 @@
+import { atom, selector } from 'recoil';
+import { IArticle, IPR } from '../types/dataTypes';
+
+const per_page = 10;
+
+export const articleListAtom = atom<IArticle[]>({
+  key: 'articleList',
+  default: [],
+});
+
+export const currentPage = atom<number>({
+  key: 'currentPage',
+  default: 1,
+});
+
+const authorList = [
+  'Q',
+  'Daisy',
+  'goody',
+  'adela',
+  'Seong',
+  'eamon',
+  'Tami',
+  'autumn',
+  'eve',
+  'swing',
+];
+
+export const filterIndexAtom = atom<number[]>({
+  key: 'filterIndex',
+  default: [],
+});
+
+const bodyParser = ({ body, base, user, number }: IPR) => {
+  const info = body.split('|');
+  if (info.length !== 16) return { id: -1 };
+  const link = info[14].match(/\([^)]+\)/)?.[0].replace(/[\(\)]/g, '');
+  return {
+    id: number,
+    link: link || '잘못된 링크입니다.',
+    condition: parseInt(info[13].trim()),
+    userImgUrl: user.avatar_url,
+    userName: base.ref,
+    date: info[11],
+  };
+};
+
+export const fetchData = selector<IArticle[]>({
+  key: 'fetchDataSelector',
+  get: async ({ get }) => {
+    const page = get(currentPage);
+
+    const filterFetch = async (filterIndex: number) => {
+      const author = authorList[filterIndex];
+      const response = await fetch(
+        `https://api.github.com/repos/mico-members/miracle-coding/pulls?state=closed&per_page=${per_page}&page=${page}&base=${author}`,
+      );
+      const data = await response.json();
+      return data.map(bodyParser).filter(({ id }: { id: number }) => id !== -1);
+    };
+
+    const defaultFetch = async () => {
+      const response = await fetch(
+        `https://api.github.com/repos/mico-members/miracle-coding/pulls?state=closed&per_page=${per_page}&page=${page}`,
+      );
+      const data = await response.json();
+      return data.map(bodyParser).filter(({ id }: { id: number }) => id !== -1);
+    };
+
+    const reducer = async (acc: Promise<IArticle[]>, cur: number) => {
+      const awaitedAcc = await acc.then();
+      const data = await filterFetch(cur);
+      const result: IArticle[] = [...awaitedAcc, ...data];
+      return result;
+    };
+
+    return get(filterIndexAtom).length === 0
+      ? defaultFetch()
+      : (await get(filterIndexAtom).reduce(reducer, Promise.resolve([]))).sort(
+          (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+        );
+  },
+});

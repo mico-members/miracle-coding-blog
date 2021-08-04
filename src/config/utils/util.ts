72bae0987837865a4b958b454f4d76e693ec3d8d
Autumn (조홍비) <60209518+dyongdi@users.noreply.github.com>
2021-08-05 00:48:19 +0900

    [#18] feat: 컨디션 숫자에 따라 이모지 렌더링하는 함수 생성 (#22)

--- src/config/utils/util.ts
+++ src/config/utils/util.ts
@@ -1,14 +1,28 @@
 export function timeCalculator(created: string) {
-    const now = new Date();
-    const offset = new Date().getTimezoneOffset() * 60000;
+  const now = new Date();
+  const offset = new Date().getTimezoneOffset() * 60000;
 
-    const today = new Date(now.getTime() - offset).toISOString().slice(0, 10).split('-');
-    const createDay =  created.split('-');
-    if(today[0] === createDay[0] && today[1] === createDay[1]){
-        if(+today[2] - +createDay[2] === 0) return 'today'
-        if(+today[2] - +createDay[2] === 1) return 'yesterday'
-        if(+today[2] - +createDay[2] === 2) return '2 days ago'
-    }
+  const today = new Date(now.getTime() - offset)
+    .toISOString()
+    .slice(0, 10)
+    .split('-');
+  const createDay = created.split('-');
+  if (today[0] === createDay[0] && today[1] === createDay[1]) {
+    if (+today[2] - +createDay[2] === 0) return 'today';
+    if (+today[2] - +createDay[2] === 1) return 'yesterday';
+    if (+today[2] - +createDay[2] === 2) return '2 days ago';
+  }
 
-    return created; 
-   }
+  return created;
+}
+
+type TConditionEmoji = '😥' | '😑' | '🙂' | '😀' | '😆👍' | '❓';
+
+export function renderConditionEmoji(condition: number): TConditionEmoji {
+  if (condition >= 0 && condition <= 2) return '😥';
+  if (condition <= 4) return '😑';
+  if (condition <= 6) return '🙂';
+  if (condition <= 8) return '😀';
+  if (condition <= 10) return '😆👍';
+  return '❓';
+}

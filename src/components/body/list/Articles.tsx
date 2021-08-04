13507748cc60d48c150475474ca3713b68966f76
Seong Young Jeong <70461368+GleamingStar@users.noreply.github.com>
2021-08-04 18:18:03 +0900

    [#17] fix: BodyParser 수정 (#21)
    * fix:[#17] Add case for non-template
    
    지정 템플릿을 준수하지 않은 pr에 대한 조항 추가
    
    * fix:[#17] Add state for check fetch end
    
    데이터가 더 없음을 확인하는 isOver 속성 추가

--- src/components/body/list/Articles.tsx
+++ src/components/body/list/Articles.tsx
@@ -9,25 +9,27 @@ const Articles = () => {
   const setPageNum = useSetRecoilState(currentPage);
   const articleList = useRecoilValueLoadable(fetchData);
   const [articles, setArticles] = useState<IArticle[]>([]);
+  const [isOver, setOver] = useState<boolean>(false);
 
   const observer = useRef<IntersectionObserver>();
 
-  const lastArticleElementRef = useCallback((node) => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setPageNum((prevPageNum) => prevPageNum + 1);
-      }
-    });
-    if (node) observer.current.observe(node);
-  }, []);
+  const lastArticleElementRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && !isOver) {
+          setPageNum((prevPageNum) => prevPageNum + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isOver],
+  );
 
   useEffect(() => {
-    if (articleList.state === 'hasValue') {
-      setArticles((articles) => {
-        return [...articles, ...articleList.contents];
-      });
-    }
+    if (articleList.state === 'hasValue')
+      if (articleList.contents.length === 0) setOver(true);
+      else setArticles((articles) => [...articles, ...articleList.contents]);
   }, [articleList]);
 
   return (
@@ -51,7 +53,5 @@ const ArticlesWrapper = styled.ul`
   ${({ theme }) => theme.style.flexColumn};
   gap: 1rem;
 `;
-
-
 
 export default Articles;

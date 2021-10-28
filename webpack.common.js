89b0c048026909055556b9398ea1c23a7cc84a9f
Hongbi <dyongdi@gmail.com>
2021-10-29 02:30:01 +0900

    [#43] WIP: webpack 설정 파일 분리, 현재 빌드 안 됨
    - 이유는 번들 파일의 크기가 커서
    - 번들 파일의 크기를 줄이는 작업 필요

--- webpack.common.js
+++ webpack.common.js
@@ -4,10 +4,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
   output: {
-    filename: 'app.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
@@ -26,21 +25,14 @@ module.exports = {
       },
     ],
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    host: 'localhost',
-    port: process.env.PORT || 8080,
-    open: true,
-    historyApiFallback: true,
-  },
   plugins: [
     new Dotenv({
       path: './.env',
-      safe: true,
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      title: 'Production',
     }),
   ],
 };
---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    host: 'localhost',
+    port: process.env.PORT || 8080,
+    open: true,
+    historyApiFallback: true,
+  },
+});
